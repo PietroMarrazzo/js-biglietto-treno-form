13e4ffbd0515dc61ff72a13d8cb3c05b4e6302af
Pietro <marrazzo.pietroalfonso@gmail.com>
2020-10-21 14:50:19 +0200

    starting js

---
+++ js/main.js
@@ -0,0 +1,16 @@
+// reference elementi
+var biglietto = document.getElementById("biglietto");
+var btnGenera = document.getElementById("genera");
+var btnAnnulla = document.getElementById("annulla");
+
+// calcolo biglietto e comparsa risultato
+btnGenera.addEventListener('click', function()) {
+    
+    var nome = document.getElementById('username').value;
+    var percorrenza = document.getElementById('percorrenza').value;
+    var eta = document.getElementById('fascia-eta').value;
+    var prezzoBase = 0.21;
+
+    // calcolo
+}
+
