9136817df980378539b7c469d63986217dbb8dc8
Pietro <marrazzo.pietroalfonso@gmail.com>
2020-10-21 15:49:56 +0200

    javascript working

--- js/main.js
+++ js/main.js
@@ -6,6 +6,7 @@ var btnAnnulla = document.getElementById("annulla");
 // calcolo biglietto e comparsa risultato
 btnGenera.addEventListener('click', function() {
     
+    // reference biglietto
     var nome = document.getElementById('username').value;
     var percorrenza = document.getElementById('percorrenza').value;
     var eta = document.getElementById('fascia-eta').value;
@@ -13,18 +14,55 @@ btnGenera.addEventListener('click', function() {
     var scontoU18 = 0.2;
     var scontoO65 = 0.3; 
 
-    // calcolo
+    // calcolo prezzo biglietto
     var prezzo = prezzoBase * percorrenza;
     var offerta = 'Biglietto Standard';
 
-    // calcolo sconto
-    if (eta === 'minorenne') {
-        
+    // calcolo eventuale sconto
+    if (eta === 'U18') {
         prezzo -= prezzo * scontoU18; 
+        offerta = 'Biglietto Under 18';
     } 
-    else if (eta === 'Over 65') {
+    else if (eta === 'O65') {
         prezzo -= prezzo * scontoO65; 
+        offerta = 'Biglietto Over 65';
     }
+    prezzo = prezzo.toFixed(2) + ' €';
+    
+    // generatore carrozza e codice treno
+    var codTreno = Math.floor((Math.random() * 1000) + 9000);
+    var nCarrozza = Math.floor((Math.random() * 10) + 1);
+
+    // display biglietto e info 
+    if (nome == '') {
+        biglietto.className = 'hidden';
+        alert('Compila il modulo del tuo nome per validare il biglietto');
+        
+    } else
+    biglietto.className = 'display';
+    document.getElementById('nome').innerHTML = nome;
+    document.getElementById('offerta').innerHTML = offerta;
+    document.getElementById('carrozza').innerHTML = nCarrozza;
+    document.getElementById('codice').innerHTML = codTreno;
+    document.getElementById('costo').innerHTML = prezzo;
+
+});
+
+// bottone reset
+btnAnnulla.addEventListener('click', function() {
+
+    // display biglietto e info 
+    biglietto.className = 'hidden';
+    document.getElementById('nome').innerHTML = '';
+    document.getElementById('offerta').innerHTML = '';
+    document.getElementById('carrozza').innerHTML = '';
+    document.getElementById('codice').innerHTML = '';
+    document.getElementById('costo').innerHTML = '';
+
+    document.getElementById('username').value = '';
+    document.getElementById('percorrenza').value = '';
+    document.getElementById('fascia-eta').value = '';
+
 
 });
 
