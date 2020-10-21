54e4705a1ef104c4e2e6788b3a2105313bed3392
Pietro <marrazzo.pietroalfonso@gmail.com>
2020-10-21 15:05:19 +0200

    calcolo biglietto

--- js/main.js
+++ js/main.js
@@ -4,13 +4,27 @@ var btnGenera = document.getElementById("genera");
 var btnAnnulla = document.getElementById("annulla");
 
 // calcolo biglietto e comparsa risultato
-btnGenera.addEventListener('click', function()) {
+btnGenera.addEventListener('click', function() {
     
     var nome = document.getElementById('username').value;
     var percorrenza = document.getElementById('percorrenza').value;
     var eta = document.getElementById('fascia-eta').value;
     var prezzoBase = 0.21;
+    var scontoU18 = 0.2;
+    var scontoO65 = 0.3; 
 
     // calcolo
-}
+    var prezzo = prezzoBase * percorrenza;
+    var offerta = 'Biglietto Standard';
+
+    // calcolo sconto
+    if (eta === 'minorenne') {
+        
+        prezzo -= prezzo * scontoU18; 
+    } 
+    else if (eta === 'Over 65') {
+        prezzo -= prezzo * scontoO65; 
+    }
+
+});
 
